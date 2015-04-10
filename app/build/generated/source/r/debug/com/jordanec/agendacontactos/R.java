c476277f87a490b0b22894711feae19e0b0bf8ad
JordanEC <JordanEC@users.noreply.github.com>
2015-04-10 16:49:21 -0600

    Theme changed

--- app/build/generated/source/r/debug/com/jordanec/agendacontactos/R.java
+++ app/build/generated/source/r/debug/com/jordanec/agendacontactos/R.java
@@ -1317,17 +1317,17 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f08002e;
         public static final int action_mode_bar_stub=0x7f08002d;
         public static final int action_mode_close_button=0x7f080020;
-        public static final int action_settings=0x7f080054;
+        public static final int action_settings=0x7f080053;
         public static final int activity_chooser_view_content=0x7f080021;
         public static final int always=0x7f080016;
         public static final int beginning=0x7f080013;
-        public static final int btnAgregar=0x7f080043;
-        public static final int btnBuscar=0x7f080042;
-        public static final int btnCancelar=0x7f08004e;
-        public static final int btnEditar=0x7f080053;
-        public static final int btnEliminar=0x7f080044;
+        public static final int btnAgregar=0x7f080042;
+        public static final int btnBuscar=0x7f080041;
+        public static final int btnCancelar=0x7f08004d;
+        public static final int btnEditar=0x7f080052;
+        public static final int btnEliminar=0x7f080043;
         public static final int btnListar=0x7f080040;
-        public static final int btnPositivo=0x7f08004f;
+        public static final int btnPositivo=0x7f08004e;
         public static final int checkbox=0x7f080029;
         public static final int collapseActionView=0x7f080017;
         public static final int decor_content_parent=0x7f08002f;
@@ -1337,13 +1337,13 @@ containing a value of this type.
         public static final int dropdown=0x7f08001c;
         public static final int edit_query=0x7f080033;
         public static final int end=0x7f080014;
-        public static final int etApellido1=0x7f080048;
-        public static final int etApellido2=0x7f080049;
-        public static final int etCasa=0x7f08004b;
-        public static final int etCelular=0x7f08004a;
-        public static final int etDireccion=0x7f08004c;
-        public static final int etIDContacto=0x7f080046;
-        public static final int etNombre=0x7f080047;
+        public static final int etApellido1=0x7f080047;
+        public static final int etApellido2=0x7f080048;
+        public static final int etCasa=0x7f08004a;
+        public static final int etCelular=0x7f080049;
+        public static final int etDireccion=0x7f08004b;
+        public static final int etIDContacto=0x7f080045;
+        public static final int etNombre=0x7f080046;
         public static final int expand_activities_button=0x7f080022;
         public static final int expanded_menu=0x7f080028;
         public static final int home=0x7f080004;
@@ -1353,8 +1353,8 @@ containing a value of this type.
         public static final int image=0x7f080023;
         public static final int listMode=0x7f080009;
         public static final int list_item=0x7f080025;
-        public static final int llBotones=0x7f08004d;
-        public static final int llContactoItem=0x7f080050;
+        public static final int llBotones=0x7f08004c;
+        public static final int llContactoItem=0x7f08004f;
         public static final int lvListarContactos=0x7f08003f;
         public static final int middle=0x7f080015;
         public static final int never=0x7f080019;
@@ -1380,11 +1380,10 @@ containing a value of this type.
         public static final int split_action_bar=0x7f080007;
         public static final int submit_area=0x7f08003c;
         public static final int tabMode=0x7f08000b;
-        public static final int textView=0x7f080041;
-        public static final int textView2=0x7f080045;
+        public static final int textView2=0x7f080044;
         public static final int title=0x7f080027;
-        public static final int tvIDContacto=0x7f080051;
-        public static final int tvNombreApellido1=0x7f080052;
+        public static final int tvIDContacto=0x7f080050;
+        public static final int tvNombreApellido1=0x7f080051;
         public static final int up=0x7f080008;
         public static final int useLogo=0x7f080012;
         public static final int withText=0x7f08001a;
@@ -1475,7 +1474,8 @@ containing a value of this type.
         public static final int toastKey=0x7f0a002b;
     }
     public static final class style {
-        /**  Customize your theme here. 
+        /**  Theme.AppCompat.Light.DarkActionBar 
+ Customize your theme here. 
          */
         public static final int AppTheme=0x7f0b0000;
         public static final int Base_TextAppearance_AppCompat=0x7f0b0001;
